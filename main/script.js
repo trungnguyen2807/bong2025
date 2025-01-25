419831827eca9bb0e2c1c4b027611337cb9336ce
Nguyen <lmtn2707@gmail.com>
2025-01-25 19:25:25 +1000

    3

--- main/script.js
+++ main/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     const link = document.getElementById("myLink");
     link.classList.add("show"); // Sử dụng lớp CSS để hiển thị lại
-  }, 5000);
+  }, 10000);
   console.log("Successfully loaded");
 });
 
